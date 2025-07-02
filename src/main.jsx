4cd4c8b61ab082060a3a15957c19c62b8a0cb2a3
Developerkaran <developerkaran11@gmail.com>
2025-07-03 01:27:55 +0530

    🎬 Initial commit: MovieHunter app with search + debounce + TMDB API.

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Analytics } from "@vercel/analytics/react";
-
+import { inject } from "@vercel/analytics";
+inject();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
